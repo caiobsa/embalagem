5b724c4ebe5fa0d675757ef7b5a7b90bcae13cb1
caiobsa <caiobsa@gmail.com>
2023-11-03 18:03:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,90 @@
 import streamlit as st
 import pandas as pd
+from io import BytesIO
 
-# Configuração de estilo para aumentar a fonte
+# Função para gerar o arquivo Excel
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False)
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+# Inicializa o histórico no estado da sessão se ainda não estiver lá
+if 'historico' not in st.session_state:
+    st.session_state.historico = []
+
+# Lista de embalagens padrão disponíveis no Brasil (exemplos)
+# Substitua estas dimensões pelas dimensões reais das embalagens que você tem
+embalagens_padrao = [
+    (16, 11, 6),
+    (18, 13, 9),
+    (22, 16, 10),
+    # ... continue adicionando embalagens
+]
+
+# Função para encontrar a embalagem adequada (substitua esta função pela sua função correta)
+def encontrar_embalagem(comprimento, largura, altura, peso):
+    # Esta é uma função de exemplo, substitua-a pela sua lógica de negócios
+    for embalagem in embalagens_padrao:
+        if embalagem[0] >= comprimento and embalagem[1] >= largura and embalagem[2] >= altura:
+            return embalagem
+    return None
+
+# Estilos personalizados
 st.markdown("""
 <style>
 .big-font {
-    font-size:20px !important;
+    font-size:30px !important;
+}
+input[type="number"] {
+    -webkit-appearance: none;
+    margin: 0;
+    width: 120px;
+    height: 120px;
+    font-size: 22px;
+    text-align: center;
 }
 </style>
 """, unsafe_allow_html=True)
 
-# Título do app
+# Sidebar (agora à direita)
+st.sidebar.title("Histórico de Cálculos")
+if st.session_state.historico:
+    historico_df = pd.DataFrame(st.session_state.historico)
+    st.sidebar.table(historico_df)
+
+# Botão para baixar o histórico em Excel
+if st.sidebar.button("Baixar histórico em Excel"):
+    df_excel = pd.DataFrame(st.session_state.historico)
+    excel_file = to_excel(df_excel)
+    st.sidebar.download_button(label="📥 Baixar Excel",
+                               data=excel_file,
+                               file_name='historico_embalagens.xlsx')
+
+# Layout principal
 st.title('Calculadora de Embalagens para E-commerce')
 
-# Sidebar para histórico de cálculos
-st.sidebar.header("Histórico de Cálculos")
-historico = st.sidebar.empty()  # Placeholder para o histórico
+with st.form(key='my_form'):
+    comprimento = st.number_input('Comprimento (cm)', min_value=0, format='%d')
+    largura = st.number_input('Largura (cm)', min_value=0, format='%d')
+    altura = st.number_input('Altura (cm)', min_value=0, format='%d')
+    peso = st.number_input('Peso (g)', min_value=0, format='%d')
+    submit_button = st.form_submit_button(label='Calcular embalagem ideal')
 
-# Dicionário para armazenar o histórico
-historico_dict = {"Comprimento": [], "Largura": [], "Altura": [], "Peso": [], "Embalagem Sugerida": []}
-
-# Lista de embalagens padrão disponíveis no Brasil (exemplos)
-embalagens = [
-    (16, 11, 6),
-    (18, 13, 9),
-    (44, 31, 11),
-    (20, 14, 10),
-    (20, 14, 14),
-    (30, 20, 20),
-    (30, 22, 22),
-    # Adicione aqui mais tamanhos de embalagens conforme necessário
-]
-
-# Campos para inserção das dimensões e peso do produto
-comprimento = st.number_input('Comprimento do produto (cm):', min_value=0, format='%d', key="comprimento")
-largura = st.number_input('Largura do produto (cm):', min_value=0, format='%d', key="largura")
-altura = st.number_input('Altura do produto (cm):', min_value=0, format='%d', key="altura")
-peso = st.number_input('Peso do produto (g):', min_value=0, format='%d', key="peso")
-
-# Função para encontrar a embalagem ideal
-def encontrar_embalagem(comprimento, largura, altura, peso):
-    # Aqui você pode implementar uma lógica mais complexa para escolher a embalagem com base nas dimensões e peso
-    # Vamos apenas selecionar a primeira embalagem que seja maior que o produto em todas as dimensões
-    for emb in embalagens:
-        if emb[0] >= comprimento and emb[1] >= largura and emb[2] >= altura:
-            return emb
-    return None  # Caso nenhuma embalagem seja adequada
-
-# Botão para calcular a embalagem
-if st.button('Calcular embalagem ideal', key="calcular"):
+if submit_button:
     embalagem = encontrar_embalagem(comprimento, largura, altura, peso)
     if embalagem:
         resultado = f'Use a embalagem: {embalagem[0]}x{embalagem[1]}x{embalagem[2]} cm'
         st.markdown(f'<p class="big-font">{resultado}</p>', unsafe_allow_html=True)
-        
-        # Adicionar ao histórico
-        historico_dict["Comprimento"].append(comprimento)
-        historico_dict["Largura"].append(largura)
-        historico_dict["Altura"].append(altura)
-        historico_dict["Peso"].append(peso)
-        historico_dict["Embalagem Sugerida"].append(f'{embalagem[0]}x{embalagem[1]}x{embalagem[2]} cm')
-        
-        # Atualiza a tabela no sidebar
-        historico_df = pd.DataFrame(historico_dict)
-        historico.dataframe(historico_df)
+        # Adiciona ao histórico no estado da sessão
+        st.session_state.historico.append({
+            "Comprimento": comprimento,
+            "Largura": largura,
+            "Altura": altura,
+            "Peso": peso,
+            "Embalagem Sugerida": f'{embalagem[0]}x{embalagem[1]}x{embalagem[2]} cm'
+        })
     else:
-        st.markdown('<p class="big-font">Produto muito pesado ou grande, consulte nossa equipe para uma solução personalizada.</p>', unsafe_allow_html=True)
-
-# Botão para baixar o histórico em Excel
-if st.sidebar.button("Baixar histórico em Excel"):
-    historico_df = pd.DataFrame(historico_dict)
-    towrite = pd.ExcelWriter("historico_embalagens.xlsx")
-    historico_df.to_excel(towrite, index=False)
-    towrite.save()
-    towrite.close()
-    st.sidebar.download_button(
-         label="Baixar Excel",
-         data=open("historico_embalagens.xlsx", "rb"),
-         file_name="historico_embalagens.xlsx",
-         mime="application/vnd.ms-excel"
-    )
-
-# Este comando garante que o app não rode novamente automaticamente a cada interação
-st.session_state.run_once = True
+        st.markdown('<p class="big-font">Não foi possível encontrar uma embalagem padrão adequada.</p>', unsafe_allow_html=True)
