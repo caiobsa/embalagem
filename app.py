d8a51b776323abc603005758c2c7ef839a16a066
caiobsa <caiobsa@gmail.com>
2023-11-03 18:11:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,37 +2,12 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
-# Função para gerar o arquivo Excel
-def to_excel(df):
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False)
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
-
-# Inicializa o histórico no estado da sessão se ainda não estiver lá
-if 'historico' not in st.session_state:
-    st.session_state.historico = []
-
-# Lista de embalagens padrão disponíveis no Brasil (exemplos)
-embalagens_padrao = [
-    (16, 11, 6),
-    (18, 13, 9),
-    (22, 16, 10),
-    # ... continue adicionando embalagens
-]
-
-# Função para encontrar a embalagem adequada (substitua esta função pela sua função correta)
-def encontrar_embalagem(comprimento, largura, altura, peso):
-    for embalagem in embalagens_padrao:
-        if embalagem[0] >= comprimento and embalagem[1] >= largura and embalagem[2] >= altura:
-            return embalagem
-    return None
-
-# Estilos personalizados para campos de input quadrados
+# Configurações de estilo - mantidas conforme seu código original
 st.markdown("""
 <style>
+.big-font {
+    font-size:22px !important;
+}
 input[type="number"] {
     -webkit-appearance: none;
     margin: 0;
@@ -45,33 +20,63 @@ div.row-widget.stNumberInput > div {flex-direction: column;}
 </style>
 """, unsafe_allow_html=True)
 
-# Layout principal
+# Função para gerar o arquivo Excel
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False)
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+# Função para encontrar a embalagem adequada (deve substituir pela função correta)
+def encontrar_embalagem(comprimento, largura, altura, peso):
+    # Substitua por sua lógica de encontro de embalagens
+    for embalagem in embalagens:
+        if embalagem[0] >= comprimento and embalagem[1] >= largura and embalagem[2] >= altura:
+            return embalagem
+    return None
+
+# Lista de embalagens padrão disponíveis no Brasil (exemplos)
+embalagens = [
+    # Adicione mais tamanhos conforme necessário
+    (16, 11, 6),
+    (18, 13, 9),
+    (44, 31, 11),
+    # ...
+]
+
+# Inicializa o histórico no estado da sessão se ainda não estiver lá
+if 'historico' not in st.session_state:
+    st.session_state['historico'] = []
+
 st.title('Calculadora de Embalagens para E-commerce')
 
-with st.form(key='my_form'):
-    comprimento = st.number_input('Comprimento (cm)', min_value=0, format='%d', key='comprimento')
-    largura = st.number_input('Largura (cm)', min_value=0, format='%d', key='largura')
-    altura = st.number_input('Altura (cm)', min_value=0, format='%d', key='altura')
-    peso = st.number_input('Peso (g)', min_value=0, format='%d', key='peso')
+# Campos para inserção das dimensões e peso do produto
+with st.form(key='dimensoes_form'):
+    comprimento = st.number_input('Comprimento (cm)', min_value=0, format='%d')
+    largura = st.number_input('Largura (cm)', min_value=0, format='%d')
+    altura = st.number_input('Altura (cm)', min_value=0, format='%d')
+    peso = st.number_input('Peso (g)', min_value=0, format='%d')
     submit_button = st.form_submit_button(label='Calcular embalagem ideal')
 
 if submit_button:
     embalagem = encontrar_embalagem(comprimento, largura, altura, peso)
     if embalagem:
         resultado = f'Use a embalagem: {embalagem[0]}x{embalagem[1]}x{embalagem[2]} cm'
+        st.markdown(f'<p class="big-font">{resultado}</p>', unsafe_allow_html=True)
         # Adiciona ao histórico no estado da sessão
         st.session_state.historico.append({
             "Comprimento": comprimento,
             "Largura": largura,
             "Altura": altura,
             "Peso": peso,
-            "Embalagem Sugerida": f'{embalagem[0]}x{embalagem[1]}x{embalagem[2]} cm'
+            "Embalagem Sugerida": resultado
         })
-        st.success(resultado)
     else:
         st.error('Não foi possível encontrar uma embalagem padrão adequada.')
 
-# Barra lateral para histórico
+# Mostra o histórico na barra lateral
 st.sidebar.title("Histórico de Cálculos")
 if st.session_state.historico:
     historico_df = pd.DataFrame(st.session_state.historico)
@@ -79,8 +84,8 @@ if st.session_state.historico:
 
     # Botão para baixar o histórico em Excel
     if st.sidebar.button("Baixar histórico em Excel"):
-        df_excel = pd.DataFrame(st.session_state.historico)
-        excel_file = to_excel(df_excel)
+        df_excel = to_excel(historico_df)
         st.sidebar.download_button(label="📥 Baixar Excel",
-                                   data=excel_file,
-                                   file_name='historico_embalagens.xlsx')
+                                   data=df_excel,
+                                   file_name='historico_embalagens.xlsx',
+                                   mime='application/vnd.ms-excel')
