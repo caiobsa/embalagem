3e0adef071f6d7fcc4b7df36565bb037e3e797af
caiobsa <caiobsa@gmail.com>
2023-11-03 19:39:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-# Configurações de estilo - mantidas e aprimoradas conforme suas solicitações
+# Configurações de estilo
 st.markdown("""
 <style>
 .big-font {
@@ -22,13 +22,12 @@ label {font-size: 18px !important;}
 
 # Função para encontrar a embalagem adequada
 def encontrar_embalagem(comprimento, largura, altura, peso):
-    # Substitua por sua lógica de encontro de embalagens
     for embalagem in embalagens:
         if embalagem[0] >= comprimento and embalagem[1] >= largura and embalagem[2] >= altura:
             return embalagem
     return None
 
-# Lista de embalagens padrão disponíveis no Brasil (exemplos)
+# Lista de embalagens padrão
 embalagens = [
     (16, 11, 6),
     (18, 13, 9),
@@ -36,13 +35,11 @@ embalagens = [
     # ... Adicione mais tamanhos conforme necessário
 ]
 
-# Inicializa o histórico no estado da sessão se ainda não estiver lá
 if 'historico' not in st.session_state:
     st.session_state['historico'] = []
 
 st.title('Calculadora de Embalagens para E-commerce')
 
-# Campos para inserção das dimensões e peso do produto
 with st.form(key='dimensoes_form'):
     comprimento = st.number_input('Comprimento (cm)', min_value=0, format='%d')
     largura = st.number_input('Largura (cm)', min_value=0, format='%d')
@@ -55,7 +52,6 @@ if submit_button:
     if embalagem:
         resultado = f'Use a embalagem: {embalagem[0]}x{embalagem[1]}x{embalagem[2]} cm'
         st.markdown(f'<p class="big-font">{resultado}</p>', unsafe_allow_html=True)
-        # Adiciona ao histórico no estado da sessão
         st.session_state['historico'].append({
             "Comprimento": comprimento,
             "Largura": largura,
@@ -66,13 +62,11 @@ if submit_button:
     else:
         st.error('Não foi possível encontrar uma embalagem padrão adequada.')
 
-# Mostra o histórico na barra lateral
+# Histórico na barra lateral
 st.sidebar.title("Histórico de Cálculos")
 if st.session_state['historico']:
     historico_df = pd.DataFrame(st.session_state['historico'])
     st.sidebar.table(historico_df)
-
-    # Botão para baixar o histórico em CSV
     if st.sidebar.button("Baixar histórico em CSV"):
         csv = historico_df.to_csv(index=False)
         st.sidebar.download_button(label="📥 Baixar CSV",
